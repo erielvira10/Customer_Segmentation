73f4b280e3c8b3c394174220f28a8739267e1d70
Eri Elvira <65483196+erielvira10@users.noreply.github.com>
2021-09-25 02:11:58 -0800

    add

---
+++ analisa_hasil_cluster_size.r
@@ -0,0 +1,15 @@
+pelanggan <- read.csv("file.txt", sep="\t")
+pelanggan_matrix <- data.matrix(pelanggan[c("Jenis.Kelamin", "Profesi", "Tipe.Residen")])
+pelanggan <- data.frame(pelanggan, pelanggan_matrix)
+Profesi <- unique(pelanggan[c("Profesi","Profesi.1")])
+Jenis.Kelamin <- unique(pelanggan[c("Jenis.Kelamin","Jenis.Kelamin.1")])
+Tipe.Profesi <- unique(pelanggan[c("Tipe.Residen","Tipe.Residen.1")])
+pelanggan$NilaiBelanjaSetahun <- pelanggan$NilaiBelanjaSetahun/1000000
+field_yang_digunakan = c("Jenis.Kelamin.1", "Umur", "Profesi.1", "Tipe.Residen.1","NilaiBelanjaSetahun")
+
+set.seed(100)
+segmentasi <- kmeans(x=pelanggan[field_yang_digunakan], centers=5, nstart=25)
+pelanggan$cluster <- segmentasi$cluster
+
+which(pelanggan$cluster == 1)
+length(which(pelanggan$cluster == 2))
